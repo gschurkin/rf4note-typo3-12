2a712ef0e405f4b0737d42e2f46576b495d7189c
gschurkin <gschurkin@gmail.com>
2024-01-17 14:22:00 +0100

    updated dependencies

--- gs_rf4note/ext_emconf.php
+++ gs_rf4note/ext_emconf.php
@@ -11,7 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-13.99.99',
+            'typo3' => '12.4.0-12.99.99',
+            'femanager' => '8.0.1-8.99.99'
         ],
         'conflicts' => [],
         'suggests' => [],
